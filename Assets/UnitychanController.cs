7f1911b99ff551a4ff03521f5df0f7e3113801c7
20060519 <91940216+20060519@users.noreply.github.com>
2021-11-04 20:30:28 +0900

    unitychanを移動させるスクリプトの追加

--- Assets/UnitychanController.cs
+++ Assets/UnitychanController.cs
@@ -12,6 +12,8 @@ public class UnitychanController : MonoBehaviour
     private float velocityZ = 16f;
     //横方向の速度
     private float velocityX = 10f;
+    // 上方向の速度
+    private float velocityY = 10f;
     //横方向に移動できる範囲
     private float movableRange = 3.4f;
 
@@ -33,18 +35,43 @@ public class UnitychanController : MonoBehaviour
     {
         //横方向の入力による速度
         float inputVelocityX = 0;
+        //上方向の入力による速度
+        float inputVelocityY = 0;
+
         //Unityちゃんを矢印キーまたはボタンに応じて左右に移動させる
         if (Input.GetKey(KeyCode.LeftArrow) && -this.movableRange < this.transform.position.x) 
         {
             //左方向への速度を代入
-            inputVelocityX = this.velocityX;
+            inputVelocityX = -this.velocityX;
         }
-        else if (Input.GetKey(KeyCode.RightArrow) && -this.transform.position.x > this.movableRange)
+        else if (Input.GetKey(KeyCode.RightArrow) && this.transform.position.x < this.movableRange)
         {
             //右方向への速度を代入
             inputVelocityX = this.velocityX;
         }
+
+        //ジャンプしていない時にスペースが押されたらジャンプする
+        if (Input.GetKeyDown(KeyCode.Space) && this.transform.position.y > 0.5f )
+        {
+            //ジャンプアニメを再生
+            this.myAnimator.SetBool("Jump", true);
+            //上方向の速度を代入
+            inputVelocityY = this.velocityY;
+        }
+
+        else 
+        {
+            //現在のY軸の値を代入
+            inputVelocityY = this.myRigidbody.velocity.y;
+        }
+
+        //Jumpステートの場合はJumpにFalseをセットする
+        if (this.myAnimator.GetCurrentAnimatorStateInfo(0).IsName("Jump"))
+        {
+            this.myAnimator.SetBool("Jump", false);
+        }
+
         //unitychanに速度を与える
-        this.myRigidbody.velocity = new Vector3(inputVelocityX, 0, velocityZ);
+        this.myRigidbody.velocity = new Vector3(inputVelocityX, inputVelocityY, velocityZ);
     }
 }
